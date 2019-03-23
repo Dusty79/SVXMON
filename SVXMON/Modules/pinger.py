7beb5b89607bc2839eb1905b7bf42c8ff95ccea2
Dusty79 <Dusty79@gmail.com>
2019-03-23 23:28:43 +0300

    Create pinger.py

---
+++ SVXMON/Modules/pinger.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import sys, subprocess
+
+if __name__ == "__main__":
+	while True:
+		argline = sys.stdin.readline()
+		argline = argline.rstrip('\n')
+		do_search = True
+		args = ['ping', '-c', '10', argline]
+		ping_process = subprocess.Popen(args, shell=False, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+		while do_search:
+			outline = ping_process.stdout.readline()
+			outline = outline.rstrip()
+			if ping_process.poll() is not None:
+				do_search = False
+				print('Ping is done.')
+				sys.stdout.flush()
+			elif outline != '':
+				print('Pinger: \n' + outline)
+				sys.stdout.flush()
+				outline = ''
